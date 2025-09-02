8526eeba23b3bf57355798cb25f8b842c00f097c
Nishi Shah <nishahh26@gmail.com>
2025-09-02 10:53:19 +0530

    Update que5.c

--- que5.c
+++ que5.c
@@ -5,21 +5,17 @@ printf("enter gross salary\n");
 int gs,n,d;
 scanf("%d",&gs);
 
-if(gs>20000)
-{
+if(gs>20000){
     d=0.15*gs;
     n=gs-d;
-    printf("the net salary is %d",n);
-}
-else if(gs>10000)
-{
+    printf("the net salary is %d",n);}
+else if(gs>10000){
     d=0.1*gs;
     n=gs-d;
-    printf("the net salary is %d",n);
-}
-else
-{
+    printf("the net salary is %d",n);}
+else{
     d=0.05*gs;
     n=gs-d;
     printf("the net salary is %d",n);}
+return 0;
 }

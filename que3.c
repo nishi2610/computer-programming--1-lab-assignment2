068f08374f5ba2598804a2a25f108bce81bb83c0
Nishi Shah <nishahh26@gmail.com>
2025-09-02 10:52:28 +0530

    Update que3.c

--- que3.c
+++ que3.c
@@ -12,11 +12,10 @@ if(gs>10000)
     n=gs+a-d;
     printf("the net salary is %d",n);
 }
-if(gs>5000)
-{
+if(gs>5000){
     a=0.07*gs;
     d=0.02*gs;
     n=gs+a-d;
-    printf("the net salary is %d",n);
-}
+    printf("the net salary is %d",n);}
+return 0;
 }

01ea92fddc40b83116558eadee09a3265bde4e5b
Nishi Shah <nishahh26@gmail.com>
2025-09-02 10:52:56 +0530

    Update que4.c

--- que4.c
+++ que4.c
@@ -4,12 +4,9 @@ void main()
 printf("enter number\n");
 int a;
 scanf("%d",&a);
-if(a%7==0)
-{
-    printf("the number is divided by 7");
-}
-else
-{
-    printf("the number is not divided by 7");
-}
+if(a%7==0){
+    printf("the number is divided by 7");}
+else{
+    printf("the number is not divided by 7");}
+return 0;
 }
